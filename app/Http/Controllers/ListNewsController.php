cd94ef15ccc78be4b57af2d50ca9e37294432558
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-02-29 15:22:37 -0300

    Atualização do controller para cadastrar uma notícia

--- app/Http/Controllers/ListNewsController.php
+++ app/Http/Controllers/ListNewsController.php
@@ -46,4 +46,24 @@ class ListNewsController extends Controller
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
+
+    public function store(Request $request)
+    {
+        try {
+
+            $data = $request->validate([
+                'titulo' => 'string|max:255|required',
+                'descricao' => 'string|max:255|required',
+                'texto_completo' => 'string|required',
+                'imagem' => 'string|required',
+                'data_de_publicacao' => 'date_format:Y-m-d|required',
+            ]);
+
+            $exercise = ListNews::create($data);
+
+            return $exercise;
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
+    }
 }
